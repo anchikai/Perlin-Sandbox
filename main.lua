70acc90d6834975c6f9535727ab84868e5349f70
goldenstein64 <goldnstein64@gmail.com>
2023-01-11 01:22:30 -0500

    Changed math.map so it doesnt return a function

--- main.lua
+++ main.lua
@@ -148,15 +148,12 @@ function math.angle(x1, y1, x2, y2)
 	return math.atan2(y2 - y1, x2 - x1)
 end
 
+---@param value number
 ---@param min1 number
 ---@param max1 number
 ---@param min2 number
 ---@param max2 number
----@return fun(value: number): number
-function math.map(min1, max1, min2, max2)
-	local interval1 = max1 - min1
-	local interval2 = max2 - min2
-	return function(value)
-		return (value - min1) * interval2 / interval1 + min2
-	end
+---@return number
+function math.map(value, min1, max1, min2, max2)
+	return (value - min1) * (max2 - min2) / (max1 - min1) + min2
 end
